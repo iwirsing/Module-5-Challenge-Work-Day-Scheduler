f6d2a44f3d4c1d640f1b2da320e71be7932cc447
iwirsing <ivymolina@gmail.com>
2022-10-28 15:15:31 -0500

    added the Saved option to show when data is saved, cleaned up JS, CSS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,98 @@
-//capture the DOM elements
-//var for input text
-var Text9 =document.getElementById("text9");
-
-//var for button
-var saveButtonEl=$('.saveBtn');
+//initialize variables and capture DOM elements
 var delButtonEl=$('.delBtn');
-var buttonIdentity;
+var saveButtonEl=$('.saveBtn');
 
 //storage array
 var toDoObjectStorage=[];
 
-
-
 //time on the title
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
-console.log(today);
 
-
+//FUNCTION 1: Running Time
+//changing time on the title
 function updateTime(){
     var timeNow = moment();
     $("#currentTime").text("at "+ timeNow.format("hh:mm:ss A "));
 }
 updateTime();
-window.setInterval(updateTime, 1000);
+window.setInterval(updateTime, 1000); //time changes every second
 
 
-
-//color code the timeblocks according to the current time
-//capture the hour
-var currentHour=Number(today.format("HH"));
-console.log(currentHour);
-
-for (var i=9;i<18;i++){
-    //create table id and use it as a selector
-    var tableId="#"+i.toString();
-    console.log(tableId);
-    if (currentHour===i){ $(tableId).addClass('present');}
-    else if (currentHour>i){$(tableId).addClass('past');}
-    else if (currentHour<i) {$(tableId).addClass('future');}
+//FUNCTION 2: Color Code 
+//color code the time blocks according to the current time
+function colorCode(){
+    //capture the hour
+    var currentHour=Number(today.format("HH"));
+    for (var i=9;i<18;i++){
+        //create table id string and use it as a selector
+        var tableId="#"+i.toString();
+        
+        //color coding conditions
+        if (currentHour===i){ $(tableId).addClass('present');}
+        else if (currentHour>i){$(tableId).addClass('past');}
+        else if (currentHour<i) {$(tableId).addClass('future');}
+        }
 }
+colorCode();
+//checks color code every minute
+window.setInterval(colorCode(),60000); 
 
-//enter data into timeblock
 
-
-//wait for save event to save data into timeblock
+//FUNCTION 3: Save
+//user enters data into time block
+//wait for save event to save data into time block
 saveButtonEl.on('click',function (event){
-    console.log("got clicked the save");
 
-    //capture the button that was clicked
-    console.log(event.target);
-    console.log(event.target.id);
-    console.log($(event.target).attr('data-button'));
-    
+    //capture the identity of the button that was clicked
     var buttonIdentityString = $(event.target).attr('data-button');
-    console.log(buttonIdentityString);
-    buttonIdentityNumber=Number($(event.target).attr('data-button'));
-    console.log(buttonIdentityNumber);
-  
-    // capture the input of that timeblock
+
     //build the id for input
     var inputIdBuild="#text"+buttonIdentityString;
-    console.log(inputIdBuild);
-    //capture text inside
+  
+    // capture the input text of that time block
     var textInside=$(inputIdBuild).val();
-    console.log(textInside);
+  
     //save to local storage
     localStorage.setItem(inputIdBuild,JSON.stringify(textInside));
+
     // display what is stored
     displayToDo();
-
-
+    
+    //change the  Save to Saved for 1.5 seconds then revert back
+    var $this = $(this);
+    $this.text('Saved').css({'color':'#FFC300'});
+    setTimeout(() => {
+        $this.html('&#10004; Save').css({'color':'white'});
+    }, 1500);
 });
+
+//FUNCTION 4: Display
 //display the stored data
 function displayToDo(){
     for (let i=9;i<18;i++){
-        //buildID string
+
+        //buildID string to go through the stored data as a key
         var IdString="#text"+i;
+
         //capture stored string
         var toDoText=JSON.parse(localStorage.getItem(IdString));
-        console.log(toDoText);
 
-        //check if there is any data
+        //check if there is any data, if there is, display the data
         if (toDoText!= undefined){
             $(IdString).val(toDoText);
         }
     }
-    
 }
-
 displayToDo()
 
-//create button to delete local storage
+//FUNCTION 5: Delete
+//delete local storage
 delButtonEl.on('click', function (event){
 
     var buttonIdentityString = $(event.target).attr('data-button');
     console.log(buttonIdentityString);
+
     //build the ID
     var inputIdBuild="#text"+buttonIdentityString;
     console.log(inputIdBuild);
@@ -108,6 +105,5 @@ delButtonEl.on('click', function (event){
 
      // display what is stored
      displayToDo();
-
 
 })
